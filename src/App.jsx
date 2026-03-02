d45b1e7e2d547b28e693f4c29836cdb97b1f233a
scriptMang <aperalta686@gmail.com>
2026-03-02 14:20:34 -0500

    Add props to the country-card component in app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,44 @@
 import CountryCard from './components/CountryCard/CountryCard'
+import {useState} from 'react'
 import './App.css'
 
-// const countryCardProps = {
-//     flagUrl: "",
-//     commonName: "",
-//     nativeName: "",
-//     population: 0,
-//     region: "",
-//     subRegion: "", 
-//     capital: "",
-//     topLevelDomain: "",
-//     currencies: [], // string_arr
-//     languages: []  // string_arr
+const countryCardProps = {
+    flagUrl: "",
+    commonName: "dog",
+    nativeName: "",
+    population: 0,
+    region: "",
+    subRegion: "", 
+    capital: "",
+    topLevelDomain: "",
+    currencies: [], // string_arr
+    languages: []  // string_arr
 
-// }
+}
 
 
 
 function App() {
+const [country, setCountry] = useState(countryCardProps);
+
+// setCountry(prevCountry =>  ({
+//   ...prevCountry, 
+//   commonName: "dog"
+// }));
 
   return (
     <>
-    <CountryCard />
+    <CountryCard 
+      flagUrl={country.flagUrl}
+      commonName={country.commonName}
+      nativeName={country.nativeName}
+      population={country.population}
+      region={country.region}
+      subRegion={country.subRegion}
+      capital={country.capital}
+      topLevelDomain={country.topLevelDomain}
+      currencies={country.currencies}
+      languages={country.languages}/>
     </>
   )
 }
--- src/components/CountryCard/CountryCard.jsx
+++ src/components/CountryCard/CountryCard.jsx
@@ -14,7 +14,12 @@
 // }
 
 // a react component that represent a country card
-function CountryCard() {
+function CountryCard(
+    { flagUrl, commonName, nativeName,
+      population, region, subRegion, capital,
+      topLevelDomain, currencies, languages
+    }) {
+        console.log("The common name of this country is: ", commonName);
     return (
     <>
      <p>CountryCard</p>
