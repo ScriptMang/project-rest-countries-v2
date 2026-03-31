048e5e57eeab6a79176d6d9d6378b16197ea571f
scriptMang <aperalta686@gmail.com>
2026-03-31 16:00:23 -0400

    Comment out log in use-fetch-countries

--- src/api.js
+++ src/api.js
@@ -9,7 +9,7 @@ function useFetchCountries(setFetchCountries) {
                 throw new Error("response failed")
             }
             // const jsonData = await resp.json();
-            console.log(jsonData);
+            // console.log(jsonData);
             setFetchCountries(jsonData);
         } catch(err) {
             console.error("Fetch error: ", err)
