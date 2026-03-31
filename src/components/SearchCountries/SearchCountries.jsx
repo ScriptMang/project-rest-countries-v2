fa9534d2adc9e8b2b4861009af2401285bb8cd6f
scriptMang <aperalta686@gmail.com>
2026-03-31 14:09:07 -0400

    Add search field to search-countries component

--- src/components/SearchCountries/SearchCountries.jsx
+++ src/components/SearchCountries/SearchCountries.jsx
@@ -1,8 +1,13 @@
+import searchIconLight from '../../assets/search-light-icon.svg'
+
 function SearchCountries(){
     return (
-    <>
-    <p>Search Countries</p>
-    </>
+    <div id="countrySearchContainer">
+        <div id="searchContainer">
+            <img src={searchIconLight} id= "searchIcon" alt=" light mode magnifying glass" />
+            <input type="text" id="countryInput" placeholder="Search for a country..." />
+        </div>
+    </div>
     )
 }
 
