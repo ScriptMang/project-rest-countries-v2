d9570f52352383d50a5640817522d9bb56f031a5
scriptMang <aperalta686@gmail.com>
2026-03-31 13:35:36 -0400

    Change appearance toggle icon to be darkmode moon for homepage

--- src/components/TitleBar/TitleBar.jsx
+++ src/components/TitleBar/TitleBar.jsx
@@ -1,11 +1,11 @@
-import lightmodeSearchIcon from "../../assets/search-light-icon.svg"
+import darkmodeIcon from "../../assets/dark-mode-icon-light.svg"
 
 function TitleBar() {
     return (
       <div className="titleBar">
            <h1 className="webTitle">Where in the World?</h1>
            <div className="webPreference">
-             <img src={lightmodeSearchIcon} alt="dark mode moon icon" />
+             <img src={darkmodeIcon} alt="dark mode toggle moon icon" />
              <button className="webPreferenceButtonToggle" alt="dark mode">Dark Mode</button>
            </div>
          </div>
